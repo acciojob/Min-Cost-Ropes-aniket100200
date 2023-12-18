7abfc601d9825bc7679ae1ffac4f25d965500546
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-18 22:29:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	//we'll try that n2logn wala apprach..
+	let ans=0;
+	arr.sort((a,b)=>b-a);
+	while(arr.length>1){
+		let last=arr[arr.length-1];
+		let secLas=arr[arr.length-2];
+		ans+=last+secLas;
+		arr.splice(arr.length-2);
+		arr.push(last+secLas);
+		arr.sort((a,b)=>b-a);
+	}
+
+	return ans;
   
 }
 
